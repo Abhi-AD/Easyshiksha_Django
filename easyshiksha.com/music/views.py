8c8f51b7cd8b8a030125be2bc9aa0fd9472884e9
Abhi-AD <dangiabhi332@gmail.com>
2024-10-24 18:40:30 +0545

    Add templates, implement render shortcut, and handle 404 HTTP errors

--- easyshiksha.com/music/views.py
+++ easyshiksha.com/music/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
 from music.models import Albume
 
 
 # Create your views here.
 def index(request):
-    all_albums = Albume.objects.all()  # Use Album instead of Albume
-    html = ""
-    for album in all_albums:
-        url = f"/music/{album.id}/"
-        html += f'<a href="{url}">{album.albume_title}</a></br>'
-    return HttpResponse(html)
+    all_albums = Albume.objects.all()
+    context = {"all_albums": all_albums}
+    return render(request, "music/index.html", context)
 
 
 def albume_details(request, albume_id):
-    return HttpResponse(f"<h2>Details for Album id: {albume_id}</h2>")
+    try:
+        albume = Albume.objects.get(pk=albume_id)
+    except Albume.DoesNotExist:
+        raise Http404("Albume does not exist")
+    return render(request, "music/details.html", {"albume": albume})
