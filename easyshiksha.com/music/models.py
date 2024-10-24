7cafc933f495295b11938423ecea6f51b1fa8ef2
Abhi-AD <dangiabhi332@gmail.com>
2024-10-24 17:48:25 +0545

    Create models, activate models, and set up database API integration

--- easyshiksha.com/music/models.py
+++ easyshiksha.com/music/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Albume(models.Model):
+    artist = models.CharField(max_length=255)
+    albume_title = models.CharField(max_length=255)
+    genre = models.CharField(max_length=100)
+    albume_logo = models.CharField(max_length=100)
+
+
+class Song(models.Model):
+    albume = models.ForeignKey(Albume, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=100)
+    song_title = models.CharField(max_length=255)
+    
