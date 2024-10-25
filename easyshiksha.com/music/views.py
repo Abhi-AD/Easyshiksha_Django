1027547121e79971c096479f1d719bced085cfaa
Abhi-AD <dangiabhi332@gmail.com>
2024-10-25 20:57:53 +0545

    Remove hardcoded URLs, add namespaces and 404 shortcut, and create simple form

--- easyshiksha.com/music/views.py
+++ easyshiksha.com/music/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from music.models import Albume
 
 
@@ -11,8 +11,6 @@ def index(request):
 
 
 def albume_details(request, albume_id):
-    try:
-        albume = Albume.objects.get(pk=albume_id)
-    except Albume.DoesNotExist:
-        raise Http404("Albume does not exist")
+    # albume = Albume.objects.get(pk=albume_id)
+    albume = get_object_or_404(Albume, pk=albume_id)
     return render(request, "music/details.html", {"albume": albume})
